1cede1d959d7f3951e746e75bbd9dbd6bc16449b
SamT95 <samuelthompson99@outlook.com>
2024-03-13 13:08:06 +0000

    Attempt deployment without dependency layer for testing

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigw,
     aws_iam as iam,
+    aws_secretsmanager as secretsmanager
 )
 from constructs import Construct
 
@@ -11,16 +12,27 @@ class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Retrieve the Lambda Layer ARN from Secrets Manager
+        # layer_arn_secret = secretsmanager.Secret.from_secret_name_v2(self, "LambdaLayerARN", "rag-layer-arn")
+        # layer_arn = layer_arn_secret.secret_value.to_string()
+
+        # Reference existing layer for lambda function
+        # dependency_layer = _lambda.LayerVersion.from_layer_version_arn(
+        #     self, "DependencyLayer",
+        #     layer_version_arn=layer_arn
+        # )
+
         # Define the Lambda function
         self.qa_chain_lambda = _lambda.Function(
             self, "QueryChain",
             runtime=_lambda.Runtime.PYTHON_3_10,
             handler="lambda_handler.lambda_handler",  # File is `lambda_handler.py`, function is `lambda_handler`
             # Code comes from zipped deployment package
-            code=_lambda.Code.from_asset("../backend/lambda-package.zip"), # zip containing deps, `lambda_handler.py` and other necessary python files
+            code=_lambda.Code.from_asset("../backend/rag"), # Code for the RAG chain and lambda handler
             environment={
                 "SAGEMAKER_EXECUTION_ROLE": "arn:aws:iam::349382198749:role/SagemakerExecutionRoleCustom"
-            }
+            },
+            # layers=[dependency_layer],
         )
 
         # Define policy statement to allow lambda to access secrets manager
@@ -37,8 +49,18 @@ class InfraStack(Stack):
             resources=["arn:aws:sagemaker:*:*:endpoint/*"]
         )
 
+        s3_policy = iam.PolicyStatement(
+            actions=["s3:*"],
+            effect=iam.Effect.ALLOW,
+            resources=[
+                "arn:aws:s3:::sagemaker-eu-west-1-349382198749",
+                "arn:aws:s3:::sagemaker-eu-west-1-349382198749/*"
+                ]
+        )
+
         self.qa_chain_lambda.role.add_to_policy(secrets_manager_policy)
         self.qa_chain_lambda.role.add_to_policy(sagemaker_policy)
+        self.qa_chain_lambda.role.add_to_policy(s3_policy)
 
         # Define API gateway
 
