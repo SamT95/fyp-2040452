8bc3218408fd4db0913b13f6583148695897f5ad
SamT95 <samuelthompson99@outlook.com>
2024-03-16 18:47:42 +0000

    Move backend infra to infra/backend/ folder

---
+++ infra/backend/backend_stack.py
@@ -0,0 +1,61 @@
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+    aws_iam as iam,
+    aws_secretsmanager as secretsmanager,
+    aws_lambda_python_alpha as _alambda
+)
+from constructs import Construct
+
+class BackendStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.qa_chain_lambda = _alambda.PythonFunction(
+            self,
+            "QueryChain",
+            entry="../backend/rag",
+            index="lambda_handler.py",
+            handler="lambda_handler",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+        )
+
+        # Define policy statement to allow lambda to access secrets manager
+        secrets_manager_policy = iam.PolicyStatement(
+            actions=["secretsmanager:GetSecretValue"],
+            effect=iam.Effect.ALLOW,
+            resources=["*"]
+        )
+
+        # Define policy statement to allow lambda to invoke sagemaker endpoints
+        sagemaker_policy = iam.PolicyStatement(
+            actions=["sagemaker:InvokeEndpoint"],
+            effect=iam.Effect.ALLOW,
+            resources=["arn:aws:sagemaker:*:*:endpoint/*"]
+        )
+
+        s3_policy = iam.PolicyStatement(
+            actions=["s3:*"],
+            effect=iam.Effect.ALLOW,
+            resources=[
+                "arn:aws:s3:::sagemaker-eu-west-1-349382198749",
+                "arn:aws:s3:::sagemaker-eu-west-1-349382198749/*"
+                ]
+        )
+
+        self.qa_chain_lambda.role.add_to_policy(secrets_manager_policy)
+        self.qa_chain_lambda.role.add_to_policy(sagemaker_policy)
+        self.qa_chain_lambda.role.add_to_policy(s3_policy)
+
+        # Define API gateway
+
+        self.chain_api = apigw.LambdaRestApi(
+            self, "QueryChainAPI",
+            handler=self.qa_chain_lambda,
+            proxy=False,
+        )
+
+        query_resource = self.chain_api.root.add_resource("query")
+        query_resource.add_method("POST") 
+
