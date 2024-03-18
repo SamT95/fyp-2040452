99201d7c1cc772a3d6c0da8f58da4888e5bcdd08
SamT95 <samuelthompson99@outlook.com>
2024-03-18 03:32:22 +0000

    Add navbar component

---
+++ frontend/src/app/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import styles from "./Navbar.module.css";
+import DeploymentStatus from "@/app/components/DeploymentStatus/DeploymentStatus";
+
+export default function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.navbarContainer}>
+                <h1 className={styles.navbarTitle}>UP2020452 FYP</h1>
+                <h1 className={styles.navbarTitle}>Cyber Security Chatbot</h1>
+                <DeploymentStatus />
+                {/* <button className={styles.githubButton}>GitHub</button> */}
+            </div>
+        </nav>
+    );
+}
