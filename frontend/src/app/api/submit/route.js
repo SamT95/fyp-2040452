b62f0d24e29653c9e84f81fbc625af15c657ee9c
SamT95 <samuelthompson99@outlook.com>
2024-03-18 03:30:32 +0000

    Add API for submitting user query to API gateway

---
+++ frontend/src/app/api/submit/route.js
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    // Pull the message from the request body
+    const { message } = await req.json();
+    console.log("message", message);
+    // Send the message to the backend API and get a response
+    try {
+        const response = await fetch(process.env.CHAIN_API_URL, {
+            method: "POST",
+            body: JSON.stringify({ query: message }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+        console.log("Data made json", data)
+        if (data.message) {
+            console.error("Error from backend API", data.message);
+            return NextResponse.json({
+                status: 500,
+                body: {
+                    message: data.message,
+                }
+            });
+        }
+        return NextResponse.json({
+            status: 200,
+            body: {
+                message: data.result,
+                sources: data.source_documents,
+            }
+        });
+    } catch (error) {
+        console.error("Error from backend API", error);
+        return NextResponse.json({
+            status: 500,
+            body: {
+                message: "Failed to submit message.",
+                error: error.message,
+            }
+        });
+    }
+}
