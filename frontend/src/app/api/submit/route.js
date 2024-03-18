4f508ffcbf762721651e9f63a7784c3a04f0b92c
SamT95 <samuelthompson99@outlook.com>
2024-03-18 16:44:42 +0000

    Use axios instead of fetch

--- frontend/src/app/api/submit/route.js
+++ frontend/src/app/api/submit/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 
 export const dynamic = "force-dynamic";
 
@@ -7,29 +8,38 @@ export async function POST(req) {
     const { message } = await req.json();
     // Send the message to the backend API and get a response
     try {
-        const response = await fetch(`${process.env.CHAIN_API_URL}query`, {
-            method: "POST",
-            body: JSON.stringify({ query: message }),
+        const response = await axios.post(`${process.env.CHAIN_API_URL}query`, { 
+            query: message
+         }, {
             headers: {
                 "Content-Type": "application/json",
             },
+            timeout: 30000,
         });
-        const data = await response.json();
-        console.log("Response data from backend API", data)
-        if (data.message) {
-            console.error("Error from backend API", data.message);
+        console.log("Response test: ", response.data)
+        console.log("Response error: ", response.error)
+        // const response = await fetch(`${process.env.CHAIN_API_URL}query`, {
+        //     method: "POST",
+        //     body: JSON.stringify({ query: message }),
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //     },
+        // });
+        // const data = await response.json();
+        if (response.statusText !== "OK") {
+            console.error("Error from backend API", response.data.result);
             return NextResponse.json({
                 status: 500,
                 body: {
-                    message: data.message,
+                    message: response.data.result,
                 }
             });
         }
         return NextResponse.json({
             status: 200,
             body: {
-                message: data.result,
-                sources: data.source_documents,
+                message: response.data.result,
+                sources: response.data.source_documents,
             }
         });
     } catch (error) {
@@ -38,7 +48,7 @@ export async function POST(req) {
             status: 500,
             body: {
                 message: "Failed to submit message.",
-                error: error.message,
+                error: error.response.data.result,
             }
         });
     }
