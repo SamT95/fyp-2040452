f71e0eb289d9a9fb83e741e3628ad97b199c056e
SamT95 <samuelthompson99@outlook.com>
2024-03-18 03:28:46 +0000

    Add hook for submitting message to API and handling bot loading response

---
+++ frontend/src/app/hooks/useChat.js
@@ -0,0 +1,52 @@
+// Hook for sending messages in the chat window
+// and updating the Zustand store with the new message
+
+import useStore from "@/app/store/useStore";
+import { PulseLoader } from "react-spinners";
+
+export default function useChat() {
+    const messageId = useStore.getState().conversations.length + 1;
+    const addMessage = useStore((state) => state.addMessage);
+    const setLoading = useStore((state) => state.setLoading);
+    const updateMessage = useStore((state) => state.updateMessage);
+
+    async function sendMessage(messageContent) {
+        // Add user message to store
+        addMessage({ id: messageId, sender: "user", message: messageContent});
+        setLoading(true); // Set loading state to true while waiting for response
+        // Add temporaty bot loading message to store
+        const loadingMessageId = messageId + 1;
+        addMessage({ id: loadingMessageId, sender: "bot", message: <PulseLoader size={8}/>, loading: true});
+
+        // Call backend API
+        try {
+            const response = await fetch("/api/submit", {
+                method: "POST",
+                body: JSON.stringify({ message: messageContent }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const responseData = await response.json();
+            const sources = responseData.body.sources;
+            const sourceIdsAndReferences = sources.map(source => {
+                const { id, references } = source.metadata;
+                return { id, references };
+            })
+            const newMessage = responseData.body.message;
+            updateMessage(loadingMessageId - 1, {
+                message: newMessage,
+                sources: sourceIdsAndReferences,
+            }); // Update loading message with response
+            setLoading(false); // Set loading state to false after response
+        } catch (error) {
+            console.error("Failed to submit message:", error);
+            updateMessage(loadingMessageId - 1, "Sorry, something went wrong. Please try again.");
+            setLoading(false); // Set loading state to false after response
+        }
+    }
+
+    return {
+        sendMessage,
+    };
+}
