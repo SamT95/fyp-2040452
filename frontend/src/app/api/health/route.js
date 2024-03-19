cfb696ea7b9d2e9c149f9a8c8a7640b6762e95da
SamT95 <samuelthompson99@outlook.com>
2024-03-19 16:56:48 +0000

    Update health check api response

--- frontend/src/app/api/health/route.js
+++ frontend/src/app/api/health/route.js
@@ -3,8 +3,5 @@ import { NextResponse } from "next/server";
 export async function GET() {
     return NextResponse.json({
         status: 200,
-        body: {
-            message: "App is healthy",
-        },
     });
 }
