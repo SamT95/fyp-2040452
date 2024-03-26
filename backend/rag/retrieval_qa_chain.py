629becfa57e77f14090aa1342820af79f093dfc2
SamT95 <samuelthompson99@outlook.com>
2024-03-26 11:11:34 +0000

    Implement chat history

--- backend/rag/retrieval_qa_chain.py
+++ backend/rag/retrieval_qa_chain.py
@@ -4,7 +4,7 @@
 # The chain handles the embedding of the user's question, the retrieval of the most similar document, and the answering of the question.
 
 from langchain.chains import RetrievalQA
-from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from common.custom_embeddings import CustomCohereEmbeddings
 from common.fetch_keys import fetch_cohere_key
 from common.custom_vectorstore import CustomPineconeVectorstore, load_existing_index
@@ -19,6 +19,10 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+sagemaker_execution_role = os.environ.get("SAGEMAKER_EXECUTION_ROLE")
+llm = build_sagemaker_llm_endpoint(sagemaker_execution_role)
+
 def create_prompt_template():
     """
     This function creates a prompt template.
@@ -30,29 +34,62 @@ def create_prompt_template():
     prompt_template = """
     Act as a helpful cyber security expert and answer the question below.
     If you do not know the answer to the question, explain that you do not know.
-    Context is provided above the question and acts as additional information to complement your built-in knowledge.
-    Utilise the context and your knowledge to answer the question and provide citations for any context used.
+    Use the following pieces of retrieved context to answer the question.
+    Keep the answer concise and informative.
 
     Context: {context}
-
-    Question: {question}
-
-    Answer: 
     """
 
-    prompt = PromptTemplate(
-        template=prompt_template,
-        input_variables=["context", "question"],
+    system_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_template),
+            MessagesPlaceholder(variable_name="chat_history")
+            ("human", "{question}"),
+        ]
     )
 
-    return prompt
+    # prompt = PromptTemplate(
+    #     template=prompt_template,
+    #     input_variables=["context", "question"],
+    # )
+
+    return system_prompt
+
+def contextualize_prompt_and_history():
+    """
+    This function returns a chain that reformulates an incoming prompt.
+    It utilises a message placeholder for the chat history and 
+    features a system prompt that tells the model to reformulate the question.
+    """
+
+    system_prompt = """
+    Given a conversation history and the latest human user question, which might reference context
+    in the conversation history, create a standalone question which can be answered without the chat history.
+    DO NOT answer the question, just reformulate it if needed, or return it as is.
+    """
+    contextualize_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}")
+        ]
+    )
+    contextualize_prompt_chain = contextualize_prompt | llm | StrOutputParser()
+    return contextualize_prompt_chain
+
+def contextualized_prompt(input: dict):
+    context_chain = contextualize_prompt_and_history()
+    if "chat_history" in input:
+        return context_chain
+    else:
+        return input["question"]
 
 def format_docs(docs):
     for doc in docs:
         logger.info(f"Doc: {doc}")
         logger.info(f"Page content: {doc.page_content}")
         logger.info(f"Metadata: {doc.metadata}")
-    return "\n\n".join([f"{doc.page_content}" for doc in docs])
+    return "\n\n".join([f"{doc.page_content} (Metadata: {doc.metadata})" for doc in docs])
     
 def create_qa_chain():
     """
@@ -65,26 +102,37 @@ def create_qa_chain():
         model="embed-english-v3.0",
         cohere_api_key=cohere_api_key,
     )
-    sagemaker_execution_role = os.environ.get("SAGEMAKER_EXECUTION_ROLE")
-    llm = build_sagemaker_llm_endpoint(sagemaker_execution_role)
+
     index_name = "rag-index"
     pinecone_index = load_existing_index(index_name)
     text_field = "text"
     vectorstore = CustomPineconeVectorstore(pinecone_index, embeddings, text_field)
-    vectorstore_retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
     prompt = create_prompt_template()
     # handler = StdOutCallbackHandler() # Initialise an output callback handler for streaming
 
-    chain_from_docs = (
-        RunnablePassthrough.assign(context=(lambda docs: format_docs(docs["context"])))
+    rag_chain = (
+        RunnablePassthrough.assign(
+            context=contextualize_prompt_and_history | retriever | format_docs
+        )
         | prompt
         | llm
-        | StrOutputParser()
     )
 
-    chain_with_source = RunnableParallel(
-        {"context": vectorstore_retriever, "question": RunnablePassthrough()}
-    ).assign(answer=chain_from_docs)
+    return rag_chain
+
+    # chain_from_docs = (
+    #     RunnablePassthrough.assign(context=(lambda docs: format_docs(docs["context"])))
+    #     | prompt
+    #     | llm
+    #     | StrOutputParser()
+    # )
+
+    # chain_with_source = RunnableParallel(
+    #     {"context": retriever, "question": RunnablePassthrough()}
+    # ).assign(answer=chain_from_docs)
+
+
     # chain = (
     #     {"context": vectorstore.as_retriever(search_kwargs={"k": 4}) | format_docs, "question": RunnablePassthrough()}
     #     | prompt
