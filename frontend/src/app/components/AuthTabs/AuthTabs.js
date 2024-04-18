5069fd336db4d40340c28562a4edb53f57aba110
SamT95 <samuelthompson99@outlook.com>
2024-04-18 17:54:15 +0100

    Create component for displaying auth forms in tabs

---
+++ frontend/src/app/components/AuthTabs/AuthTabs.js
@@ -0,0 +1,37 @@
+"use client"
+import styles from "./AuthTabs.module.css";
+import { useState } from "react";
+import OnboardingTabs from "@/app/components/AuthForms/OnboardingTabs";
+import SignInTab from "@/app/components/AuthForms/SignInForm";
+
+export default function AuthTabs() {
+    const [activeTab, setActiveTab] = useState("signin");
+
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+    }
+
+    return (
+        <div className={styles.pageContainer}>
+            <div role="tablist" className={styles.formContainer}>
+                <div className={styles.tabButtons}>
+                    <button 
+                    onClick={() => handleTabChange("signin")}
+                    role="tab"
+                    className={`${styles.tab} ${activeTab === "signin" ? styles.active : ""}`}>
+                        Sign in
+                    </button>
+                    <button 
+                    onClick={() => handleTabChange("register")}
+                    role="tab"
+                    className={`${styles.tab} ${activeTab === "register" ? styles.active : ""}`}>
+                        Register
+                    </button>
+                </div>
+                <div className={styles.formBox}>
+                    {activeTab === "signin" ? <SignInTab /> : <OnboardingTabs />}
+                </div>
+            </div>
+        </div>
+    );
+}
