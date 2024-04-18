c2fb67e92ddde31bf527d8c0b8d3faa7969e8601
SamT95 <samuelthompson99@outlook.com>
2024-04-18 17:43:24 +0100

    Add middleware file for redirecting to auth page

---
+++ frontend/src/middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import * as jwt from "jsonwebtoken";
+
+export function middleware(request) {
+    if (!request.cookies.has("access_token")) {
+        return NextResponse.redirect(new URL("/auth", request.url));
+    } else {
+        const accessToken = request.cookies.get("access_token");
+        const decodedToken = jwt.decode(accessToken.value); // Decode (add a check for expiration)
+        const response = NextResponse.next();
+        response.headers.set("user_id", decodedToken.sub);
+        response.headers.set("user_name", decodedToken.username);
+        response.headers.set("x-url", request.url);
+        return response;
+    }
+}
+
+// Specify a matcher to only apply the middleware to the /chat route
+export const config = {
+    matcher: "/chat"
+}
